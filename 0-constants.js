2eef6e19774fe4502bdbd9f703221299fed4320f
Obichukwu9 <123229717+Obichukwu9@users.noreply.github.com>
2024-10-04 00:29:32 +0100

    Update 0-constants.js

--- 0-constants.js
+++ 0-constants.js
@@ -3,13 +3,8 @@ export function taskFirst() {
   return task;
 }
 
-export function getLast() {
-  return ' is okay';
-}
-
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
